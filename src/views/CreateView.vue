7d81d59c55c5d11de6a285f334c88e0eca0548d5
unknown <ezegeek@gmail.com>
2023-06-22 15:44:56 -0300

    Minor Fixes

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
         <b-card bg-variant="light">
-            <b-form-group label-cols-lg="3" label="Create visitor" label-size="lg" label-class="font-weight-bold pt-0"
-                class="mb-0">
+            <b-form-group :invalid-feedback="invalidFeedback" :state="state" label-cols-lg="3" label="Create visitor"
+                label-size="lg" label-class="font-weight-bold pt-0" class="mb-0">
                 <b-form-group label-cols="4" label-cols-lg="2" label="Name" label-for="input-default">
                     <b-form-input id="input-default" v-model="newVisitName" @keydown.enter="addVisit"></b-form-input>
                 </b-form-group>
@@ -13,6 +13,12 @@
             <b-button variant="outline-danger" class="mt-3 mx-2" @click="goBack">Cancel</b-button>
             <b-button variant="primary" class="mt-3" @click="addVisit">Create</b-button>
         </b-card>
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+        </p>
     </div>
 </template>
 
@@ -23,35 +29,49 @@ export default {
         return {
             newVisitName: "",
             newVisitLastname: "",
+            errors: [],
         }
     },
-    components: {
+    computed: {
+        state() {
+            return this.newVisitName.length >= 3 && this.newVisitLastname.length >= 3
+        },
+        invalidFeedback() {
+            if (this.newVisitName.length > 0 && this.newVisitName.length < 3) {
+                return 'Enter a name that has at least 3 characters.'
+            }
 
+            if (this.newVisitLastname.length > 0 && this.newVisitLastname.length < 3) {
+                return 'Enter a lastname that has at least 3 characters.'
+            }
+        }
     },
     methods: {
         async addVisit() {
+            if (this.state) {
+                let newVisit = {
+                    firstname: this.newVisitName,
+                    lastname: this.newVisitLastname,
+                };
 
-            let newVisit = {
-                firstname: this.newVisitName,
-                lastname: this.newVisitLastname,
-            };
+                await fetch(import.meta.env.VITE_API_URL + "/visitors", {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(newVisit)
+                })
 
-            await fetch("https://0slrsqlw38.execute-api.sa-east-1.amazonaws.com/visitors", {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(newVisit)
-            })
-
-            this.newVisitName = "";
-            this.newVisitLastname = "";
-            this.$router.back()
+                this.newVisitName = "";
+                this.newVisitLastname = "";
+                this.$router.back();
+            }
         },
         goBack() {
             console.log('Creation canceled');
             this.$router.back();
-        }
+        },
+
     }
 }
 </script>
